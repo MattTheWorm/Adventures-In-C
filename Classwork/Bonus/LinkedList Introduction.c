61533dee91ad820cc8d65318a96d3fb2c3b67740
MattTheWorm <Matttheworm@gmail.com>
2019-11-18 16:43:14 -0500

    added printhelp function

--- Classwork/Bonus/LinkedList Introduction.c
+++ Classwork/Bonus/LinkedList Introduction.c
@@ -18,13 +18,41 @@ struct Part{
 //End
 //Function Declaration
 
-
+void printHelp();
 
 //End
 int main(){
 
-    printf("Hello World!");
+    printf("LinkedList session begin.\n");
+    printHelp();
     return 0;
+
+}
+
+void printHelp(){
+
+    int i;
+    for(i = 0; i != 86; i++){//85 Dashes
+        putchar('-');
+
+    }
+
+    putchar('\n');
+    puts(" What would you like to do?\n");
+    printf("\t\"create\"%6s-Create a new part list.\n", "");
+    printf("\t\"print\"%7s-Print the current part list if it exists.\n", "");
+    printf("\t\"select\"%6s-Print an existing part within the current list.\n", "");
+    printf("\t\"modify\"%6s-Modify an existing part within the current list.\n", "");
+    printf("\t\"delete\"%6s-Delete an existing part within the current list.\n", "");
+    printf("\t\"insert\"%6s-Insert a new part into the current list.\n", "");
+    printf("\t\"save\"%8s-Save the part list to a file if it exists.\n", "");
+    printf("\t\"add\"%9s-Accept a file of parts and append them to the current list.\n", "");
+    printf("\t\"exit\"%8s-Exit the program.\n\n", "");
+    printf("\t\"help\"%8s-Open this menu.\n\n", "");
+    for(i = 0; i != 86; i++){//85 Dashes
+        putchar('-');
+
+    }
 
 }
 
