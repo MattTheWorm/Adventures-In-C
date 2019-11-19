1808bbec06ce88b931f7e5e9f9c256d5c946dd02
MattTheWorm <Matttheworm@gmail.com>
2019-11-18 19:39:29 -0500

    big changes (see desc)
    created the skeleton for the createList function (including validation), added function comments

--- Classwork/Bonus/LinkedList Introduction.c
+++ Classwork/Bonus/LinkedList Introduction.c
@@ -19,16 +19,23 @@ struct Part{
 //Function Declaration
 
 void printHelp();
+void createList(struct Part *head);
 
 //End
 int main(){
+    struct Part Part1 = {10, 10, 9.99, NULL};
+    struct Part *head = &Part1;
 
     printf("LinkedList session begin.\n");
     printHelp();
+    createList(head);
     return 0;
 
 }
 
+//////////
+// printHelp (C)
+//////////
 void printHelp(){
 
     int i;
@@ -49,8 +56,48 @@ void printHelp(){
     printf("\t\"add\"%9s-Accept a file of parts and append them to the current list.\n", "");
     printf("\t\"exit\"%8s-Exit the program.\n\n", "");
     printf("\t\"help\"%8s-Open this menu.\n\n", "");
-    for(i = 0; i != 86; i++){//85 Dashes
+    for(i = 0; i != 86; i++){
         putchar('-');
+
+    }
+
+    putchar('\n');
+
+}
+//////////
+// createList
+//////////
+void createList(struct Part *head){
+    char yesNo = 0;
+
+    if(!head){
+        puts("[Create]: No head pointer detected, assuming new list.");
+
+    }
+    else{
+        puts("[Create]: List detected. Do you want to save the current list to file? [Y/N]");
+        while(!yesNo){
+            fputs("Input: ", stdout);
+            yesNo = getchar();
+            while(getchar() != '\n'); //Rid ALL extra characters.
+
+            switch(yesNo){
+                case 'Y': case 'y':
+                    //printf("User chose yes.\n");
+                    break;
+
+                case 'N': case 'n':
+                    //printf("User chose no.\n");
+                    break;
+
+                default:
+                    yesNo = 0;
+                    puts("Invalid input. --> [Y or N]\n");
+
+            }
+        }
+
+        printf("Head Data\nNum: %d\nQuant: %d\nPrice: $%.2lf\nNextPtr: %p", head->num, head->quantity, head->price, head->next);
 
     }
 
