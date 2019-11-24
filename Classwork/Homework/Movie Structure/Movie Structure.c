1810d41bc8afb206538ab56f65931c67aab3f2dd
MattTheWorm <Matttheworm@gmail.com>
2019-11-24 02:30:09 -0500

    Created basic source file for starting next assignment
    along with import data

---
+++ Classwork/Homework/Movie Structure/Movie Structure.c
@@ -0,0 +1,29 @@
+/*| Matt J. | Started on: 11/24/19 @1:47AM | CIS-170 |
+
+Desc: Movie Structure CSV Assignment.
+
+*/
+#include <stdio.h>
+//Struct Declaration
+
+struct Movie{
+    char name[128];
+    char mpaa[4];
+    int minutes;
+    float ratingOne, ratingTwo, ratingThree;
+
+};
+
+//End
+//Function Declaration
+
+
+
+//End
+int main(){
+    struct Movie[10];
+
+    printf("Hello World!");
+    return 0;
+
+}
