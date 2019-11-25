eca9ff87463082ad8dc1be6109412653273b922e
MattTheWorm <Matttheworm@gmail.com>
2019-11-24 22:55:40 -0500

    implemented basic file reading

--- Classwork/Homework/Movie Structure/Movie Structure.c
+++ Classwork/Homework/Movie Structure/Movie Structure.c
@@ -7,8 +7,8 @@ Desc: Movie Structure CSV Assignment.
 //Struct Declaration
 
 struct Movie{
-    char name[128];
-    char mpaa[4];
+    char name[129];
+    char mpaa[6];
     int minutes;
     float ratingOne, ratingTwo, ratingThree;
 
@@ -21,9 +21,53 @@ struct Movie{
 
 //End
 int main(){
-    struct Movie[10];
+    FILE *importFile;
+    FILE *exportFile;
+    struct Movie movieArray[10];
+    char path[257];
+    int i;
 
-    printf("Hello World!");
+    puts("Enter the target import path.");
+    i = 0;
+    while(1){//Path input loop
+        fputs("Input: ", stdout);
+        fgets(path, sizeof(path), stdin);
+        while(path[i]){
+            if(path[i] == '\n'){
+                //puts("Newline found.");
+                path[i] = '\0';
+
+            }
+
+            i++;
+
+        }
+
+        //printf("User entered: %s", path);
+        if(!(importFile = fopen(path, "r"))){
+            puts("That file failed to open. Try again or exit the program.");
+            //printf("File pointer: %p\n", (void *)importFile);
+            continue;
+
+        }
+
+        puts("Import file found.");
+        break;
+
+    }
+    //
+    //File Import
+    //
+    char fileLine[128], token;
+
+    puts("---------");//10 Dashes
+    while(fgets(fileLine, sizeof(fileLine), importFile)){
+        //Name, Rating, Runtime, Rate1, Rate2, Rate3
+        printf("Line data: %s", fileLine);
+
+    }
+
+    puts("---------");//10 Dashes
     return 0;
 
 }
