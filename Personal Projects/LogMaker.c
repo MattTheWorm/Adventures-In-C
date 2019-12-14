63cf9d01ed2c116bdb893f55472c2fd7ea39e831
MattTheWorm <Matttheworm@gmail.com>
2019-12-14 02:55:49 -0500

    Update LogMaker.c

--- Personal Projects/LogMaker.c
+++ Personal Projects/LogMaker.c
@@ -18,23 +18,56 @@ typedef struct tm tm;
 //End
 int main(){
     FILE *filePtr;
-    time_t currentTime = time(0);
-    tm *myTime = localtime(&currentTime);
-    int test;
 
-    printf("Hello World! Current time & date: %d/%d/%d", myTime->tm_mon + 1, myTime->tm_mday, myTime->tm_year + 1900);
+
     return 0;
 
 }
 
 //////////
-// newLog | Creates a new list. If the list exists, attempt to save. | WIP
+// newLog | Set-up for making a new log. | WIP
 //////////
 
-void newLogCreation(FILE *filePtr){
+void newLogCreation(FILE **filePtrPtr){
+    time_t currentTime;
+    tm *myTime;
+    char yesNo, logName[129], date;
 
 
+    puts("Welcome Matt!\nSet log name? (Y / N)");
+    while(1){
+        fputs("Input: ", stdout);
+        scanf("%c", &yesNo);
+        while(getchar() != '\n'); //Clear buffer
+        switch(yesNo){
+            case 'Y': case 'y':
+                fputs("Alright, enter the log name: ", stdout);
+                fgets(logName, sizeof(logName), stdin);//Assume there will be no overflow..
+                break;
 
+            case 'N': case 'n':
+                break;
 
+            default:
+                puts("Something went wrong, try again. (Invalid input?)");
+                continue;
+
+            //
+
+        }
+
+        break;
+
+    }
+
+    currentTime = time(0);
+    myTime = localtime(&currentTime);
+    if(!(*filePtrPtr = fopen(strcat(logName, ".txt"), "w"))){
+        puts("Log creation failed! Sorry, program exiting.");
+        exit(EXIT_FAILURE);
+
+    }
+
+    printf(("Log creation ready! %s Date: %d/%d/%d\n", yesNo = (yesNo == 'y') ? logName : "(No name)", myTime->tm_mon + 1, myTime->tm_mday, myTime->tm_year);
 
 }
