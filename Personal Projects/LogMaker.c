932c2902ac12462ce0185f6b2bc1b2dafab63dd9
MattTheWorm <Matttheworm@gmail.com>
2019-12-13 23:31:02 -0500

    basic time practice

--- Personal Projects/LogMaker.c
+++ Personal Projects/LogMaker.c
@@ -1,7 +1,40 @@
-#include <stdio.h>
+/*| Matt J. | Started on: 12/13/19 @7:25PM |
 
+Desc: Program to automating the creation of logs.
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Struct Declaration
+
+typedef struct tm tm;
+
+//End
+//Function Declaration
+
+
+
+//End
 int main(){
-    printf("Hello World!");
+    FILE *filePtr;
+    time_t currentTime = time(0);
+    tm *myTime = localtime(&currentTime);
+    int test;
+
+    printf("Hello World! Current time & date: %d/%d/%d", myTime->tm_mon + 1, myTime->tm_mday, myTime->tm_year + 1900);
     return 0;
+
+}
+
+//////////
+// newLog | Creates a new list. If the list exists, attempt to save. | WIP
+//////////
+
+void newLogCreation(FILE *filePtr){
+
+
+
+
 
 }
--- Personal Projects/README.md
+++ Personal Projects/README.md
@@ -1,6 +1,6 @@
 # Personal Projects (From earliest, to latest)
 **LogMaker**
 
-I'm creating this program for my logs. I figured it'd be fun/convenient to make a program to automate all of the quirks of my log formatting.
+I'm creating this program for my logs (Like a journal/log thing). I figured it'd be fun/convenient to make a program to automate all of the quirks of my log formatting.
 
 
